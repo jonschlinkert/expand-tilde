fe78eb4cf3de78d204af9fbc686eb717641dafd1
jonschlinkert <jon.schlinkert@sellside.com>
2015-01-21 21:31:24 -0500

    use path.join

--- index.js
+++ index.js
@@ -5,14 +5,15 @@
  * Licensed under the MIT license.
  */
 
+var path = require('path');
 var userhome = require('user-home');
 
 module.exports = function expandTilde(fp) {
   if (fp.charCodeAt(0) === 126 /* ~ */) {
     if (fp.charCodeAt(1) === 43 /* + */) {
-      return process.cwd() + '/' + fp.slice(2);
+      return path.join(process.cwd(), fp.slice(2));
     }
-    return userhome ? (userhome + '/' + fp.slice(1)) : fp;
+    return userhome ? path.join(userhome, fp.slice(1)) : fp;
   }
   return fp;
 };
--- test.js
+++ test.js
@@ -7,10 +7,14 @@
 
 'use strict';
 
+var path = require('path');
+path.sep = '/';
+
 var assert = require('assert');
 var expandTilde = require('./');
 
 it('should equal', function () {
   assert.equal(expandTilde('~'), '/Users/jonschlinkert');
   assert.equal(expandTilde('~+'), process.cwd());
+  assert.equal(expandTilde('~+/foo/bar'), path.join(process.cwd(), 'foo/bar'));
 });
