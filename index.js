62d1720e7cb5e8a6d358a3517206dfebf9a833bb
Wermeille Bastien <bastien.wermeille@gmail.com>
2022-03-16 07:55:45 +0100

    Refactor
    No need to compute `homedir` in every case.

--- index.js
+++ index.js
@@ -9,12 +9,11 @@ var homedir = require('homedir-polyfill');
 var path = require('path');
 
 module.exports = function expandTilde(filepath) {
-  var home = homedir();
-
   if (filepath.charCodeAt(0) === 126 /* ~ */) {
     if (filepath.charCodeAt(1) === 43 /* + */) {
       return path.join(process.cwd(), filepath.slice(2));
     }
+    const home = homedir();
     return home ? path.join(home, filepath.slice(1)) : filepath;
   }
 
